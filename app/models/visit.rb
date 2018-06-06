011da0f7107226d3af3a791cd1c63ba04c451cbd
vijayabharathi <vijayabharathib@users.noreply.github.com>
2018-06-06 23:40:21 +0530

    reorder index results to return created_date in descending order

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,6 @@
 class Visit < ApplicationRecord
 
     scope :group_by_date,
-        -> { group("created_at::date").count('id')}
+        -> { group("created_at::date").order("created_at::date DESC").count('id')}
     scope :by_domain, ->(domain) { where("domain=?",domain)}
 end
