701fcabc581e8bfa2e6aeb44a87fc53701215dfd
vijayabharathi <vijayabharathib@users.noreply.github.com>
2018-06-06 23:22:54 +0530

    limit allowed domains < fetch them from credentials :lock:

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Project
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
         resource '*', headers: :any, methods: [:get, :post, :put, :options]
       end
     end
